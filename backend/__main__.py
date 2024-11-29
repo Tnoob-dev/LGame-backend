a6be5416e038ea42baa981722228907bf053a9e4
Tnoob-dev <cristiandeleonmonzon@gmail.com>
2024-11-29 15:28:22 -0500

    added cors for frontend

--- backend/__main__.py
+++ backend/__main__.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from contextlib import asynccontextmanager
 from .src.routes import home, games
 from .src.utils import logs, db
@@ -14,12 +15,23 @@ async def lifespan(app: FastAPI):
     finally:
         log.info("App Closed")
 
+
+
 app = FastAPI(
     title="LGame-Backend",
     version="1.0.0",
     lifespan=lifespan
 )
 
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(home.router)
 app.include_router(games.router)
